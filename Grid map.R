1dfbf28905d37f7d339014117144744a496c1d13
YiFan <franyifanzhang@yahoo.com>
2018-04-27 18:29:12 -0400

    Create Grid map.R

---
+++ Grid map.R
@@ -0,0 +1,21 @@
+# ants and traffic
+# Change digit display
+
+options(digits=10)
+
+n <- 7
+m <- 11
+
+npoint <- rep(0:(m+n+1), each=2)
+xys <- rep(c("x","y"), (m+n+1))
+xycoord <- paste0(xys, npoint)
+# n times north + m times east. combination c(n, k)
+npath <- choose(18,11)
+
+# manual way
+path1 <- c(0,0,0,1
+path <- c(0,0,
+path <- c(0,0)
+path <- data.frame(
+
+rbind(x,list(1,16,"Paul"))
