bf2ab0dbf73438d8d4be836cafe5f9b81150e8b7
YiFan <franyifanzhang@yahoo.com>
2018-04-29 15:24:41 -0400

    Update Grid map.R
    add comments

--- Grid map.R
+++ Grid map.R
@@ -1,42 +1,41 @@
-# ants and traffic
+# Q1
+# This solution based on one assumption is that the ant always make progress even after it reaches the edges of the grid.
+
+# For 31 by 23 grid, there are simply too many combinations to be able to generated. Probably don't need to have all combinations generated since only the maximal distance is needed.
 # Change digit display
-
 options(digits=10)
 
-n <- 7
-m <- 11
-
-
-
-npoint <- rep(0:(m+n+1), each=2)
-xys <- rep(c("x","y"), (m+n+1))
-xycoord <- paste0(xys, npoint)
-# n times north + m times east. combination c(n, k)
-npath <- choose(18,11)
-combn(seq(1:18), 7)
-
-
-# test: start small
-options(digits=10)
-n <- 2
-m <- 3
-direc <- combn(seq(1:5), 2)
-npath <- choose((m+n),m)
-path <- matrix(0, npath, (m+n))
-for(i in 1:npath) {
-  path[i,direc[,i]] <- 1
+dist <- function(n, m){
+  # total steps = n times north + m times east
+  steps <- m+n
+  # combination c(n, k)
+  direc <- combn(seq(1:steps), n)
+  # number of total paths
+  npath <- choose(steps,n)
+  # create movement matrix for each path (north=1, east=0)
+  path <- matrix(0, npath, steps)
+  for(i in 1:npath) {
+    path[i,direc[,i]] <- 1
+  }
+  # Cartesian coordinate after each move 
+  xpath <- ypath <- matrix(0, npath, (steps+1))
+  for(i in 2:(steps+1)){
+    xpath[,i] <- xpath[,i-1] + as.integer(path[,i-1]==0)
+    ypath[,i] <- ypath[,i-1] + path[,i-1]
+  }
+  # assign column names
+  xpath <- as.data.frame(xpath)
+  ypath <- as.data.frame(ypath)
+  colnames(xpath) <- paste0("x", 0:steps)
+  colnames(ypath) <- paste0("y", 0:steps)
+  # calculate maximal distance
+  dp_dist <- abs(xpath/m - ypath/n)
+  dp_dist$max <- apply(dp_dist, 1, max)
+  return(dp_dist$max)
 }
 
-xpath <- ypath <- matrix(0, npath, (m+n+1))
+d1 <- dist(7,11)
+mean(d1)
+sqrt(sum((d1 - mean(d1))^2)/(length(d1)))
 
-for(i in 2:6){
-xpath[,i] <- xpath[,i-1] + as.integer(path[,i-1]==0)
-ypath[,i] <- ypath[,i-1] + path[,i-1]
-}
-
-xpath <- as.data.frame(xpath)
-ypath <- as.data.frame(ypath)
-colnames(xpath) <- paste0("x", 0:(m+n))
-colnames(ypath) <- paste0("y", 0:(m+n))
-dp_dist <- abs(xpath/m - ypath/n)
-dp_dist$max <- apply(dp_dist, 1, max)
+sum(d1 > 0.6)/sum(d1 > 0.2)
